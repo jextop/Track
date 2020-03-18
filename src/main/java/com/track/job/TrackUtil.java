366cc4e856ede5c8119c91ad70c60c713433ac88
dingxl <dingxl>
2020-03-18 13:29:03 +0800

    close #3 按照泊松分布模拟启动30个客户端

--- src/main/java/com/track/job/TrackUtil.java
+++ src/main/java/com/track/job/TrackUtil.java
@@ -1,6 +1,7 @@
 package com.track.job;
 
 import com.track.util.MacUtil;
+import com.track.util.PoissonUtil;
 import org.apache.commons.math3.distribution.PoissonDistribution;
 import org.quartz.CronScheduleBuilder;
 import org.quartz.JobBuilder;
@@ -16,6 +17,7 @@ import java.util.List;
 
 public class TrackUtil {
     private static final String CRON = "0/7 * * * * ?";
+    private static final double LAMBDA = 3.25;
     private static final int COUNT = 30;
     private static final List<JobDetail> JOB_LIST;
 
@@ -44,28 +46,35 @@ public class TrackUtil {
 
     public static void start() {
         CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(CRON);
-        PoissonDistribution distribution = new PoissonDistribution(15);
+        PoissonDistribution distribution = new PoissonDistribution(3.25);
 
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            for (int i = 0; i < JOB_LIST.size(); i++) {
-                JobDetail job = JOB_LIST.get(i);
 
-                // 泊松分布启动客户端
-                int j = (int) (distribution.probability(i) * 1000);
-                if (j > 0) {
-                    Thread.sleep(j);
+            // 泊松分布启动客户端
+            int sum = 0;
+            do {
+                int i = sum;
+                sum += PoissonUtil.getVariable(LAMBDA);
+                for (; i < sum && i < COUNT; i++) {
+                    JobDetail job = JOB_LIST.get(i);
+
+                    // 任务之间增加随机间隔
+                    int j = (int) (Math.random() * 1000);
+                    if (j > 0) {
+                        Thread.sleep(j);
+                    }
+                    System.out.printf("启动客户端: %s, 间隔: %d\n", job.getKey().getName(), j);
+
+                    // 配置任务
+                    Trigger trigger = TriggerBuilder.newTrigger()
+                            .forJob(job)
+                            .withSchedule(scheduleBuilder)
+                            .build();
+
+                    scheduler.scheduleJob(job, trigger);
                 }
-                System.out.printf("启动客户端: %s, 间隔: %d\n", job.getKey().getName(), j);
-
-                // 配置任务
-                Trigger trigger = TriggerBuilder.newTrigger()
-                        .forJob(job)
-                        .withSchedule(scheduleBuilder)
-                        .build();
-
-                scheduler.scheduleJob(job, trigger);
-            }
+            } while (sum <= COUNT);
 
             // 开启任务
             scheduler.start();
--- src/test/java/com/track/util/PoissonUtilTest.java
+++ src/test/java/com/track/util/PoissonUtilTest.java
@@ -1,15 +1,21 @@
 package com.track.util;
 
-import org.apache.commons.math3.distribution.PoissonDistribution;
 import org.junit.Test;
 
 public class PoissonUtilTest {
     @Test
-    public void testPoisson() {
-        PoissonDistribution distribution = new PoissonDistribution(15);
+    public void testVariable() {
+        for (int i = 0; i < 10; i++) {
+            int ret = PoissonUtil.getVariable(3.25);
+            LogUtil.info(String.format("%d", ret));
+        }
+    }
+
+    @Test
+    public void testProbability() {
         double sum = 0;
-        for (int i = 0; i < 30; i++) {
-            double ret = distribution.probability(i);
+        for (int i = 0; i < 10; i++) {
+            double ret = PoissonUtil.getProbability(i, 3.25);
             sum += ret;
             LogUtil.info(String.format("%2d, %.5f", i, ret));
         }
