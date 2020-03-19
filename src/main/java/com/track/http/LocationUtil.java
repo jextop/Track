d3a5330a0bffef0bd06224d6ad6014824e6888de
dingxl <dingxl>
2020-03-19 13:13:04 +0800

    update locationUtil to use cache

--- src/main/java/com/track/http/LocationUtil.java
+++ src/main/java/com/track/http/LocationUtil.java
@@ -6,7 +6,20 @@ import com.track.util.StrUtil;
 import org.apache.commons.lang3.ArrayUtils;
 
 public class LocationUtil {
+    private static JSONObject location = null;
+
     public static JSONObject getLocation() {
+        if (location == null) {
+            synchronized (LocationUtil.class) {
+                if (location == null) {
+                    location = getLoc();
+                }
+            }
+        }
+        return location;
+    }
+
+    private static JSONObject getLoc() {
         String address = HttpUtil.sendHttpGet("http://whois.pconline.com.cn/ipJson.jsp");
         if (StrUtil.isEmpty(address)) {
             return null;
