1ae7e4170bc9e565f40005fd30dd1e44b321c0b4
dingxl <dingxl>
2020-03-30 19:30:53 +0800

    优化启动定时器

--- src/main/java/com/track/job/TrackJob.java
+++ src/main/java/com/track/job/TrackJob.java
@@ -7,15 +7,41 @@ import com.track.http.LocationUtil;
 import com.track.http.RespJsonObj;
 import com.track.util.JsonUtil;
 import com.track.util.LogUtil;
+import com.track.util.MacUtil;
+import com.track.util.PoissonUtil;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 public class TrackJob implements Job {
+    private static final double LAMBDA = 3.25;
+    private static final int COUNT = 30;
+
     @Override
     public void execute(JobExecutionContext var1) throws JobExecutionException {
-        String uid = var1.getJobDetail().getKey().getName();
-        final JSONObject location = LocationUtil.getLocation();
+        // 泊松分布启动客户端
+        int sum = 0;
+        do {
+            int i = sum;
+            sum += PoissonUtil.getVariable(LAMBDA);
+            for (; i < sum && i < COUNT; i++) {
+                sendInfo(String.format("%s%02d", MacUtil.gtMacAddr(), i + 1));
+
+                // 任务之间增加随机间隔
+                int j = (int) (Math.random() * 1000);
+                if (j > 0) {
+                    try {
+                        Thread.sleep(j);
+                    } catch (InterruptedException e) {
+                        System.err.println(e.getMessage());
+                    }
+                }
+            }
+        } while (sum <= COUNT);
+    }
+
+    private void sendInfo(String uid) {
+        JSONObject location = LocationUtil.getLocation();
         LogUtil.info("Send position", uid, JsonUtil.toStr(location));
 
         // 发送位置信息
--- src/main/java/com/track/job/TrackUtil.java
+++ src/main/java/com/track/job/TrackUtil.java
@@ -1,84 +1,59 @@
 package com.track.job;
 
-import com.track.util.MacUtil;
-import com.track.util.PoissonUtil;
 import org.quartz.CronScheduleBuilder;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
+import org.quartz.JobExecutionException;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TrackUtil {
     private static final String CRON = "0/7 * * * * ?";
-    private static final double LAMBDA = 3.25;
-    private static final int COUNT = 30;
-    private static final List<JobDetail> JOB_LIST;
+    private static final JobDetail JOB;
 
     static {
-        JOB_LIST = new ArrayList<JobDetail>();
-
         // 创建任务，模拟多个客户端
-        for (int i = 0; i < COUNT; i++) {
-            JOB_LIST.add(JobBuilder.newJob(TrackJob.class)
-                    .withIdentity(String.format("%s%02d", MacUtil.gtMacAddr(), i + 1))
-                    .build()
-            );
-        }
+        JOB = JobBuilder.newJob(TrackJob.class)
+                .build();
     }
 
     public static void stop() {
-        for (JobDetail job : JOB_LIST) {
-            try {
-                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-                scheduler.deleteJob(job.getKey());
-            } catch (SchedulerException e) {
-                System.err.println(e.getMessage());
-            }
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.deleteJob(JOB.getKey());
+        } catch (SchedulerException e) {
+            System.err.println(e.getMessage());
         }
     }
 
     public static void start() {
+        // 创建定时器
         CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(CRON);
 
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 
-            // 泊松分布启动客户端
-            int sum = 0;
-            do {
-                int i = sum;
-                sum += PoissonUtil.getVariable(LAMBDA);
-                for (; i < sum && i < COUNT; i++) {
-                    JobDetail job = JOB_LIST.get(i);
+            // 配置任务
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .forJob(JOB)
+                    .withSchedule(scheduleBuilder)
+                    .build();
 
-                    // 任务之间增加随机间隔
-                    int j = (int) (Math.random() * 1000);
-                    if (j > 0) {
-                        Thread.sleep(j);
-                    }
-                    System.out.printf("启动客户端: %s, 间隔: %d\n", job.getKey().getName(), j);
-
-                    // 配置任务
-                    Trigger trigger = TriggerBuilder.newTrigger()
-                            .forJob(job)
-                            .withSchedule(scheduleBuilder)
-                            .build();
-
-                    scheduler.scheduleJob(job, trigger);
-                }
-            } while (sum <= COUNT);
+            scheduler.scheduleJob(JOB, trigger);
 
             // 开启任务
-            scheduler.start();
+            scheduler.startDelayed(1);
         } catch (SchedulerException e) {
             System.err.println(e.getMessage());
-        } catch (InterruptedException e) {
+        }
+
+        // 立即发送一次
+        try {
+            new TrackJob().execute(null);
+        } catch (JobExecutionException e) {
             System.err.println(e.getMessage());
         }
     }
